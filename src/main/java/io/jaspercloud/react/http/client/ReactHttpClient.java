be9dc62fc586692e35e7232220d46b9b9d24d2c3
jaspercloud <276726581@qq.com>
2021-11-11 17:30:58 +0800

    ReactHttpClient

--- src/main/java/io/jaspercloud/react/http/client/ReactHttpClient.java
+++ src/main/java/io/jaspercloud/react/http/client/ReactHttpClient.java
@@ -6,13 +6,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 import reactor.netty.http.client.HttpClient;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
+import reactor.netty.resources.ConnectionProvider;
 
 /**
  * ReactHttpClient
@@ -20,20 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ReactHttpClient {
 
-    private Scheduler workScheduler;
-
-    /**
-     * @param threads 逻辑处理线程数
-     */
-    public ReactHttpClient(int threads) {
-        this(Schedulers.fromExecutor(Executors.newFixedThreadPool(threads, new DefaultThreadFactory())));
-    }
-
-    /**
-     * @param workScheduler 自定义调度器
-     */
-    public ReactHttpClient(Scheduler workScheduler) {
-        this.workScheduler = workScheduler;
+    public ReactHttpClient() {
     }
 
     /**
@@ -43,7 +25,7 @@ public class ReactHttpClient {
      * @return
      */
     public AsyncMono<Response> execute(Request request) {
-        Mono<Response> mono = HttpClient.create()
+        Mono<Response> mono = HttpClient.create(ConnectionProvider.newConnection())
                 .keepAlive(true)
                 .headers(new ProcessHeader(request))
                 .request(HttpMethod.valueOf(request.method()))
@@ -51,8 +33,7 @@ public class ReactHttpClient {
                 .send((req, out) -> {
                     return out.sendByteArray(Flux.create(new ProcessRequestBody(request)));
                 })
-                .responseSingle(new ProcessResponse(request))
-                .publishOn(workScheduler);
+                .responseSingle(new ProcessResponse(request));
         return new AsyncMono<>(mono);
     }
 
@@ -65,31 +46,5 @@ public class ReactHttpClient {
      */
     public AsyncMono<Response> execute(Request request, long timeout) {
         return execute(request).timeout(timeout);
-    }
-
-    public static class DefaultThreadFactory implements ThreadFactory {
-
-        private static final AtomicInteger poolNumber = new AtomicInteger(1);
-        private final ThreadGroup group;
-        private final AtomicInteger threadNumber = new AtomicInteger(1);
-        private final String namePrefix;
-
-        public DefaultThreadFactory() {
-            group = new ThreadGroup("reactWorker");
-            namePrefix = "reactPool-" +
-                    poolNumber.getAndIncrement() +
-                    "-thread-";
-        }
-
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(group, r,
-                    namePrefix + threadNumber.getAndIncrement(),
-                    0);
-            if (t.isDaemon())
-                t.setDaemon(false);
-            if (t.getPriority() != Thread.NORM_PRIORITY)
-                t.setPriority(Thread.NORM_PRIORITY);
-            return t;
-        }
     }
 }
