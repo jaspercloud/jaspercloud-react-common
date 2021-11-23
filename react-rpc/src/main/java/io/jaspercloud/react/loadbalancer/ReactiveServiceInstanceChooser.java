882c2d8e7f7c39fb7d67db5dce7fba7b918a7d95
jaspercloud <276726581@qq.com>
2021-11-23 14:26:38 +0800

    remove

--- react-rpc/src/main/java/io/jaspercloud/react/loadbalancer/ReactiveServiceInstanceChooser.java
+++
@@ -1,9 +0,0 @@
-package io.jaspercloud.react.loadbalancer;
-
-import io.jaspercloud.react.mono.AsyncMono;
-import org.springframework.cloud.client.ServiceInstance;
-
-public interface ReactiveServiceInstanceChooser {
-
-    AsyncMono<ServiceInstance> choose(String serviceId);
-}
