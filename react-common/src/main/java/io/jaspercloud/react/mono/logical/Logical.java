1169c8fd1269eec5b0396883d9f962741c8353a9
jaspercloud <276726581@qq.com>
2022-01-26 21:59:06 +0800

    StreamRecord

--- react-common/src/main/java/io/jaspercloud/react/mono/logical/Logical.java
+++ react-common/src/main/java/io/jaspercloud/react/mono/logical/Logical.java
@@ -1,23 +1,15 @@
 package io.jaspercloud.react.mono.logical;
 
-import io.jaspercloud.react.http.client.ReactHttpClient;
 import io.jaspercloud.react.mono.AsyncMono;
 import io.jaspercloud.react.mono.ReactAsyncCall;
 import io.jaspercloud.react.mono.ReactSink;
 import io.jaspercloud.react.mono.StreamRecord;
-import io.netty.channel.Channel;
-import io.netty.channel.embedded.EmbeddedChannel;
-import okhttp3.Request;
-import okhttp3.Response;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoSink;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -28,80 +20,6 @@ public class Logical<I, O> {
 
     public Logical(List<AsyncMono<I>> monoList) {
         this.monoList = monoList;
-    }
-
-    public static <O> void main(String[] args) throws Exception {
-        Channel channel = new AsyncMono<>(Mono.create(new Consumer<MonoSink<Channel>>() {
-            @Override
-            public void accept(MonoSink<Channel> sink) {
-                sink.success(new EmbeddedChannel());
-            }
-        })).toFuture().get();
-
-        ReactHttpClient reactHttpClient = new ReactHttpClient();
-        List<String> list = Arrays.asList(
-                "https://www.baidu.com",
-                "https://www.youku.com",
-                "https://www.hao123.com"
-        );
-        List<AsyncMono<Response>> collect = list.stream().map(e -> {
-            AsyncMono<Response> asyncMono = reactHttpClient.execute(new Request.Builder().url(e).build())
-                    .timeout(30 * 1000);
-            return asyncMono;
-        }).collect(Collectors.toList());
-        List<Integer> codeList = new Logical<Response, Response>(collect).collect().then(new ReactAsyncCall<List<Response>, List<Integer>>() {
-            @Override
-            public void process(boolean hasError, Throwable throwable, List<Response> result, ReactSink<? super List<Integer>> sink) throws Throwable {
-                if (hasError) {
-                    sink.finish();
-                    return;
-                }
-                sink.success(result.stream().map(e -> e.code()).collect(Collectors.toList()));
-            }
-        }).toFuture().get();
-
-
-        new AsyncMono<>("test")
-                .then(new ReactAsyncCall<String, String>() {
-                    @Override
-                    public void process(boolean hasError, Throwable throwable, String result, ReactSink<? super String> sink) throws Throwable {
-                        sink.success(result);
-                    }
-                })
-                .subscribe(new ReactSink<String>() {
-                    @Override
-                    public void success() {
-                        System.out.println();
-                    }
-
-                    @Override
-                    public void success(String s) {
-                        System.out.println();
-                    }
-
-                    @Override
-                    public void error(Throwable throwable) {
-                        System.out.println();
-                    }
-
-                    @Override
-                    public void finish() {
-                        System.out.println();
-                    }
-                });
-
-        ArrayList<String> tmp = new ArrayList<>(list);
-        tmp.add(null);
-        String s = new Logical<String, String>(tmp.stream().map(e -> new AsyncMono<>(e)).collect(Collectors.toList()))
-                .collect().then(new ReactAsyncCall<List<String>, String>() {
-                    @Override
-                    public void process(boolean hasError, Throwable throwable, List<String> result, ReactSink<? super String> sink) throws Throwable {
-                        System.out.println();
-                    }
-                }).toFuture().get();
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        countDownLatch.await();
-        System.out.println();
     }
 
     public AsyncMono<O> selectOne(SelectOneCall<I> call) {
